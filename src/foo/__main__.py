f88b5f23e8d888690eda0cdd041f4e343c4f2ee6
kcz358 <92624596+kcz358@users.noreply.github.com>
2024-10-12 15:52:35 +0800

    Add a foo entry point, update pyproject.toml

---
+++ src/foo/__main__.py
@@ -0,0 +1,16 @@
+def func1():
+    print("Hello from func1")
+
+
+def func2():
+    print("Hello from func2")
+
+
+def main():
+    func1()
+    func2()
+
+
+if __name__ == "__main__":
+    func1()
+    func2()
