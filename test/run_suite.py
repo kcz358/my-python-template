c973692011f8991f2e2e6f5e5e495959edfcad5f
Kaichen Zhang - NTU <zhan0564@e.ntu.edu.sg>
2024-12-11 12:18:23 +0800

    Update run_suite.py

--- test/run_suite.py
+++ test/run_suite.py
@@ -1,9 +1,11 @@
-import glob
+from pathlib import Path
 
 from utils import run_unittest_files
 
+current_dir = Path(__file__).parent.resolve()
+
 if __name__ == "__main__":
-    files = glob.glob("**/test_*.py", recursive=True)
+    files = [p for p in current_dir.glob("**/test_*.py")]
 
     exit_code = run_unittest_files(files)
     exit(exit_code)
